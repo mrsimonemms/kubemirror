c7b781c8109490a766b193bb580da53ebbf5a6cb
Simon Emms <simon@simonemms.com>
2025-01-16 20:26:58 +0000

    chore: scaffold resource

--- cmd/main.go
+++ cmd/main.go
@@ -52,6 +52,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	kubemirrorsimonemmscomv1alpha1 "github.com/mrsimonemms/kubemirror/api/v1alpha1"
+	"github.com/mrsimonemms/kubemirror/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -63,6 +66,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(kubemirrorsimonemmscomv1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -214,6 +218,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.ResourceReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Resource")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
